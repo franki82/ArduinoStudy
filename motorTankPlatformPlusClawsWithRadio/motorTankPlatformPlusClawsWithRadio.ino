673069d9731d76b8ea31b372ab1dc3297f692a55
Denis <denisefimenko82@gmail.com>
2018-06-26 21:43:41 +0300

    260620182143

---
+++ motorTankPlatformPlusClawsWithRadio/motorTankPlatformPlusClawsWithRadio.ino
@@ -0,0 +1,221 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+
+RF24 radio(9, 10);
+int data[3];
+Servo servo1;
+int servoPin = 3;
+
+int enG1 = 5, enG2 = 6;
+
+int in1 = 2;
+int in2 = 4;
+
+int in3 = 7;
+int in4 = 8;
+
+int valueX, valueY, valueSpeed = 255, revValueSpeed = 255, useCamera = -1; //set revValueSpeed = 100 for low batery
+boolean isCameraLeft = false, isCameraRight = false, isCameraCenter = true;
+boolean isServoAttached = false;
+int centerPoint = 93, rightPoint = 74, leftPoint = 131, turnTimeout = 30, currentPosition = 0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+
+  radio.begin();
+  radio.setChannel(5);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.openReadingPipe(1, 0x1234567895LL);
+  radio.startListening();
+  
+  pinMode(enG1, OUTPUT);
+  pinMode(enG2, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  servo1.attach(servoPin);
+  delay(200);
+  servo1.write(centerPoint);
+  delay(200);
+  currentPosition = centerPoint;
+  servo1.detach();
+}
+
+void loop() {
+  if (radio.available()){
+    radio.read(&data, sizeof(data));
+
+    valueX = data[0];
+    valueY = data[1];
+    useCamera = data[2];
+
+    if (useCamera == -1){
+      if (isServoAttached == true){
+          servo1.detach();
+          isServoAttached = false;
+      }
+
+         if (abs(valueX)<8){
+          valueX = 0;
+        }
+    
+        if (abs(valueY)<8){
+          valueY = 0;
+          stopEngine();
+        }
+        
+        switch (valueX){
+        case 10: 
+          rightEngine();
+          break;
+        case -10: 
+          leftEngine();
+          break;
+        }
+    
+        switch (valueY){
+        case 10:
+            forwardEngine();
+          break;
+        case -10: 
+            backwardEngine();
+          break;
+        }
+    } else{
+        if (isServoAttached == false){
+          servo1.attach(servoPin);
+          isServoAttached = true;
+          //clawsToMedium();
+          }
+       if (abs(valueX)<8){
+          valueX = 0;
+        }
+
+        if (abs(valueY)<8){
+          valueY = 0;
+        }
+              
+        switch (valueX){
+        case 10: 
+          openClaws();
+          break;
+        case -10: 
+          closeClaws();
+          break;
+        }
+    
+        switch (valueY){
+        case 10: 
+            clawsToMedium();
+          break;
+        }
+    }
+   
+  }
+
+}
+
+void forwardEngine(){
+  analogWrite(enG1, valueSpeed * 0.8);
+  analogWrite(enG2, valueSpeed * 0.8);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+    
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void backwardEngine(){
+  analogWrite(enG1, valueSpeed * 0.8);
+  analogWrite(enG2, valueSpeed * 0.8);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+
+void leftEngine(){
+  analogWrite(enG1, revValueSpeed);
+  analogWrite(enG2, valueSpeed);
+  
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void rightEngine(){
+  analogWrite(enG1, valueSpeed);
+  analogWrite(enG2, revValueSpeed);
+  
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void stopEngine(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void servoSlowForward( Servo num, int startPos, int endPos, int time)
+{ 
+  num.write(startPos);
+  int duration;
+  delay(20);  
+  for ( int i=startPos; i<endPos; i++) 
+      {  num.write(i);
+         delay(time);
+      }
+}
+
+void servoSlowBackward( Servo num, int startPos, int endPos, int time)
+{ 
+  num.write(startPos);
+  int duration;
+  delay(20);  
+  for ( int i=startPos; i>endPos; i--) 
+      {  num.write(i);
+         delay(time);
+      }
+}
+
+void clawsToMedium(){
+  if (currentPosition > centerPoint){
+    servoSlowBackward(servo1,currentPosition, centerPoint, turnTimeout);
+  } else if (currentPosition < centerPoint){
+    servoSlowForward(servo1,currentPosition, centerPoint+8, turnTimeout);
+   }
+   currentPosition = centerPoint;
+ }
+
+void closeClaws(){
+  if (currentPosition < leftPoint){
+     servo1.write(currentPosition);
+     delay(turnTimeout);
+     currentPosition++;
+  }  
+}
+
+
+void openClaws(){
+  if (currentPosition > rightPoint){
+    servo1.write(currentPosition);
+    delay(turnTimeout);
+    currentPosition--;
+  }
+}
