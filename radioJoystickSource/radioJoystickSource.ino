830fdb5f1083655abe69b336bce44267732c9400
Denis <denisefimenko82@gmail.com>
2018-10-07 23:07:49 +0300

    071020182307

--- radioJoystickSource/radioJoystickSource.ino
+++ radioJoystickSource/radioJoystickSource.ino
@@ -4,7 +4,7 @@
 #include <Adafruit_SSD1306.h>
 
 RF24 radio(A2, A3);
-int data[5], dataTelemetry[4];
+int data[6], dataTelemetry[4];
 Adafruit_SSD1306 display(4);
 
 int xPin = A1;
@@ -18,12 +18,14 @@ int buttonClawsPin = 7, buttonClawsState = 0, useClaws = -1;
 unsigned long CTime01;
 unsigned long LTime01;
 int speedPin = A6, mySpeed = 0;
+int buttonVideoView = 6, buttonVideoViewState = 0, useVideoView = -1;
 
 void setup() {
   Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   pinMode(buttonStatePin, INPUT_PULLUP);
   pinMode(buttonClawsPin, INPUT_PULLUP);
+  pinMode(buttonVideoView, INPUT_PULLUP);
   pinMode(speedPin, INPUT);
   radio.begin();
   radio.setChannel(100);
@@ -39,6 +41,7 @@ void loop() {
   buttonState = digitalRead(buttonStatePin);
   buttonClawsState = digitalRead(buttonClawsPin);
   mySpeed = 255-analogRead(speedPin)/4;
+  buttonVideoViewState = digitalRead(buttonVideoView);
 
   if (buttonState == LOW){
     useCamera = -useCamera;
@@ -50,84 +53,97 @@ void loop() {
   } else if (buttonClawsState == HIGH){
     useClaws = -1;
   }
+
+  if (buttonVideoViewState == LOW){
+    useVideoView = 1;
+  } else if (buttonVideoViewState == HIGH){
+    useVideoView = -1;
+  }
   
   if (xPosition==515){
     xValueToOut = 0;
   } else if (xPosition<515){
     xValueToOut = map(xPosition, 514, 0, 0, -10);
   } else if (xPosition>515){
-    xValueToOut = map(xPosition, 516, 1023, 0, 10);
+    xValueToOut = map(xPosition, 516, 1005, 0, 10);
   }
+
+  //Serial.print("X:");
+  //Serial.println(xValueToOut);
 
   if (yPosition==503){
     yValueToOut = 0;
   } else if (yPosition<503){
     yValueToOut = map(yPosition, 503, 0, 0, 10);
   } else if (xPosition>503){
-    yValueToOut = map(yPosition, 504, 1023, 0, -10);
+    yValueToOut = map(yPosition, 504, 1005, 0, -10);
   }
+
+  //Serial.print("Y:");
+  //Serial.println(yValueToOut);
 
   data[0] = xValueToOut;
   data[1] = yValueToOut;
   data[2] = useCamera;
   data[3] = useClaws;
   data[4] = mySpeed;
+  data[5] = useVideoView;
   radio.write(&data, sizeof(data));
     
-  CTime01 = millis();
-  if (CTime01 >= (LTime01 +50)) //read pokets period
-  {
-     int duration, angleSee, handPosition, clawsPosition;
-     radio.startListening();
-     delay(20);
-     radio.read(&dataTelemetry, sizeof(dataTelemetry));
-     duration = dataTelemetry[0];
-     angleSee = dataTelemetry[1];
-     handPosition = dataTelemetry[2];
-     clawsPosition = dataTelemetry[3];
-     if (duration > 0){
-        Serial.println("----------read-telemetry------------");
-        Serial.println(duration);
-        Serial.println(angleSee);
-        display.clearDisplay();
-        display.setTextColor(WHITE);
-        display.setCursor(0,0);
-        display.setTextSize(1);
-        display.print("Distance: ");
-        display.print(duration);
-        display.print(" / ");
-        display.print(angleSee);
-//        display.setCursor(0,9);
-//        display.setTextSize(1);
-//        display.print("Ult Angle: ");
-//        display.print(angleSee);
-//        display.setCursor(0,17);
-//        display.setTextSize(1);
-//        display.print("Speed: ");
-//        display.print(mySpeed);
-
-        display.setCursor(0,9);
-        display.setTextSize(1);
-        display.print(mySpeed);
-        display.print("/");
-        display.print(handPosition);
-        display.print("/");
-        display.print(clawsPosition);
-
-        display.setCursor(0,25);
-        display.setTextSize(1);
-        display.print("Is Active: ");
-        if (useCamera == -1 && useClaws == -1){
-          display.print("movement");
-        } else if (useClaws == 1){
-          display.print("claws");
-        } else if (useCamera == 1){
-          display.print("ult view");
-        }
-        display.display();
-     }
-     LTime01 = CTime01;
-     radio.stopListening();
-  }
+  if (useVideoView == -1){
+    CTime01 = millis();
+    if (CTime01 >= (LTime01 +50)) //read pokets period
+    {
+       int duration, angleSee, handPosition, clawsPosition;
+       radio.startListening();
+       delay(20);
+       radio.read(&dataTelemetry, sizeof(dataTelemetry));
+       duration = dataTelemetry[0];
+       angleSee = dataTelemetry[1];
+       handPosition = dataTelemetry[2];
+       clawsPosition = dataTelemetry[3];
+       if (duration > 0){
+          Serial.println("----------read-telemetry------------");
+          Serial.println(duration);
+          Serial.println(angleSee);
+          display.clearDisplay();
+          display.setTextColor(WHITE);
+          display.setCursor(0,0);
+          display.setTextSize(1);
+          display.print("Distance: ");
+          display.print(duration);
+          display.print(" / ");
+          display.print(angleSee);
+          display.setCursor(0,9);
+          display.setTextSize(1);
+          display.print(mySpeed);
+          display.print("/");
+          display.print(handPosition);
+          display.print("/");
+          display.print(clawsPosition);
+  
+          display.setCursor(0,25);
+          display.setTextSize(1);
+          display.print("Is Active: ");
+          if (useCamera == -1 && useClaws == -1){
+            display.print("movement");
+          } else if (useClaws == 1){
+            display.print("claws");
+          } else if (useCamera == 1){
+            display.print("ult view");
+          }
+          display.display();
+       }
+       LTime01 = CTime01;
+       radio.stopListening();
+    }
+  } else if (useVideoView == 1) {
+      display.clearDisplay();
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.setTextSize(1);
+      display.print("No Telemetry");
+      display.display();
+    }
   
 }
